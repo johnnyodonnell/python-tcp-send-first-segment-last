651727d66be5e113e6bf7fea4903767319c7ddec
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2024-07-17 21:01:58 -0700

    Fix TCP protocol implementation

--- src/tcp_send_first_segment_last/send.py
+++ src/tcp_send_first_segment_last/send.py
@@ -1,20 +1,16 @@
+import fcntl
+import os
+import random
 import socket
 import time
 
 from tcp_send_first_segment_last.lib.disable_auto_rst import disable, cleanup
 from tcp_send_first_segment_last.lib.IP_Datagram import IP_Datagram
+from tcp_send_first_segment_last.lib.socket_reader import read_responses
 from tcp_send_first_segment_last.lib.TCP_Segment import TCP_Segment
 from tcp_send_first_segment_last.lib.TCP_Flags import TCP_Flags
 
 
-
-def get_response(sock, dst_port):
-    while True:
-        data = sock.recv(1024)
-        ip_datagram = IP_Datagram.from_bytes(data)
-        tcp_segment = ip_datagram.get_tcp_segment()
-        if tcp_segment.get_dst_port() == dst_port:
-            return ip_datagram
 
 def reset_connection(
         sock, src_addr, dst_addr, src_port, dst_port):
@@ -26,14 +22,17 @@ def reset_connection(
 
 def establish_connection(
         sock, src_addr, dst_addr, src_port, dst_port):
+    seq_num = int.from_bytes(random.randbytes(4))
+
     # Send Syn packet
     flags = TCP_Flags()
     flags.set_syn_flag(True)
-    req_segment = TCP_Segment(src_port, dst_port, 0, 0, flags)
+    req_segment = TCP_Segment(src_port, dst_port, seq_num, 0, flags)
     sock.sendall(req_segment.get_bytes(src_addr, dst_addr))
 
     # Receive Syn-Ack
-    res_dgram = get_response(sock, src_port)
+    res_dgrams = read_responses(sock, src_port)
+    res_dgram = res_dgrams[0]
     res_segment = res_dgram.get_tcp_segment()
     flags = res_segment.get_flags()
     if not flags.get_syn_flag():
@@ -69,7 +68,8 @@ def terminate_connection(
     sock.sendall(req_segment.get_bytes(src_addr, dst_addr))
 
     # Receive Fin-Ack
-    res_dgram = get_response(sock, src_port)
+    res_dgrams = read_responses(sock, src_port)
+    res_dgram = res_dgrams[0]
     res_segment = res_dgram.get_tcp_segment()
 
     if not fin_ack_received:
@@ -84,15 +84,15 @@ def terminate_connection(
 
 
 def send_first_segment_last(dst_addr, dst_port, payload):
-    src_port = 55555
+    src_port = random.randrange(49152, 65535)
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+    # https://stackoverflow.com/a/16745561/5832619
+    fcntl.fcntl(sock, fcntl.F_SETFL, os.O_NONBLOCK)
     sock.connect((dst_addr, dst_port))
     (src_addr, _) = sock.getsockname()
 
     cleanup()
-
-    reset_connection(sock, src_addr, dst_addr, src_port, dst_port)
 
     # Needed for preventing OS from resetting TCP connection
     disable(src_port)
@@ -128,8 +128,9 @@ def send_first_segment_last(dst_addr, dst_port, payload):
 
     time.sleep(5) # Wait a few seconds before closing connection
 
-    # Receive Fin-Ack
-    res_dgram = get_response(sock, src_port)
+    # Receive Ack or Fin-Ack
+    res_dgrams = read_responses(sock, src_port)
+    res_dgram = res_dgrams[-1]
     res_segment = res_dgram.get_tcp_segment()
     seq_num = res_segment.get_ack_num()
     ack_num = res_segment.get_seq_num()
