c3aab8a5c875eed2eb5216b370ed6f4df01bc6e9
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2024-07-18 23:03:42 -0700

    Use larger payload

--- src/run.py
+++ src/run.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     print("Sending...")
     payload = b"A" * 0x1100
     payload += b"\n"
-    payload = b"Hello TCP.\n"
+    # payload = b"Hello TCP.\n"
 
     # send_first_segment_last("192.168.156.10", 1234, payload)
     send_first_segment_last("127.0.0.1", 4444, payload)
